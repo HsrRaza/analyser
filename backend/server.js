ac0a9fb1758cc35d444fc264f9984f80b316a27e
Hassan Raza <hr.raza04@gmail.com>
2025-12-01 16:02:32 +0530

    code

---
+++ backend/server.js
@@ -0,0 +1,27 @@
+import express from "express";
+import cors from "cors";
+
+
+import uploadRes from "./routes/upload.route.js";
+
+const app = express();
+
+// CORS FIX
+app.use(
+  cors({
+    origin:'*',
+    methods: ["GET", "POST"],
+    allowedHeaders: ["Content-Type"],
+  })
+);
+
+app.use(express.json());
+
+// Routes
+
+app.use("/api", uploadRes);
+
+// 🚀 IMPORTANT: Do NOT use app.listen()
+// Vercel will handle the server for you.
+
+export default app;
