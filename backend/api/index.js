0b7293dbf65bba5694efaa0ec9920b67dab648ed
Hassan Raza <hr.raza04@gmail.com>
2025-12-01 21:07:00 +0530

    fix:

--- backend/api/index.js
+++ backend/api/index.js
@@ -18,9 +18,9 @@ app.use(
 app.use(express.json());
 app.use(express.urlencoded({extended:true}))
 
-app.get("/", (req, res)=>{
-res.send("hellow")
-})
+// app.get("/", (req, res)=>{
+// res.send("hello")
+// })
 
 // Routes
 
